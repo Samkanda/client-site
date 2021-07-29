e04ff81a676014b614c35857689ca3537a6b3f3c
Navpreet Kanda <52259259+Samkanda@users.noreply.github.com>
2021-07-29 12:46:30 -0400

    Basic foundation of site

---
+++ client-site/src/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import GlobalStyle from "./components/GlobalStyle";
+
+
+//import pages
+import AboutUs from "./pages/AboutUs";
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle/>
+      <AboutUs/>
+    </div>
+  );
+}
+
+export default App;
